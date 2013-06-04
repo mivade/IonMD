6d143e2b064855c1debf95e66124e91f8384a051
Michael V. DePalatis <mvd@gatech.edu>
2013-06-04 17:50:30 -0400

    Added a script to download required packages on Debian.

---
+++ getpkgs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# This is just a simple script to grab all the dependencies you need
+# on Debian wheezy. As with **NOTE THAT THIS USES SUDO, SO READ THIS
+# AND USE AT YOUR OWN RISK**
+#
+# I wanted to make this general, but if you know how to apt-pin,
+# jessie has packages for nlopt
+
+sudo aptitude install build-essential libgsl0-dbg libgsl0-dev git python-numpy python-scipy mayavi2 python-imaging
+wget http://ab-initio.mit.edu/nlopt/nlopt-2.3.tar.gz
+tar -xvzf nlopt-2.3.tar.gz
+cd nlopt-2.3
+./configure --prefix=/usr
+make
+sudo make install
+
