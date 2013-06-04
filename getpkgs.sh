ff851e7facd4fa80989eb02c1db0a9413571a6b3
Michael V. DePalatis <mvd@gatech.edu>
2013-06-04 18:06:10 -0400

    Forgot some packages

--- getpkgs.sh
+++ getpkgs.sh
@@ -7,7 +7,7 @@
 # I wanted to make this general, but if you know how to apt-pin,
 # jessie has packages for nlopt
 
-sudo aptitude install build-essential libgsl0-dbg libgsl0-dev git python-numpy python-scipy mayavi2 python-imaging
+sudo aptitude install build-essential libgsl0-dbg libgsl0-dev git python-numpy python-scipy mayavi2 python-imaging python-matplotlib python-ctypeslib
 wget http://ab-initio.mit.edu/nlopt/nlopt-2.3.tar.gz
 tar -xvzf nlopt-2.3.tar.gz
 cd nlopt-2.3
