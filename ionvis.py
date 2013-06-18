e683acdbd999cb572810cdddcc517e4a39fc6d42
Michael V. DePalatis <mvd@gatech.edu>
2013-06-18 16:32:22 -0400

    Added ability to generate CCD images from a tar archive

--- ionvis.py
+++ ionvis.py
@@ -19,6 +19,8 @@ You should have received a copy of the GNU General Public License
 along with IonMD.  If not, see <http://www.gnu.org/licenses/>.
 """
 
+import tarfile
+import os.path
 import numpy as np
 import ctypes
 import matplotlib.pyplot as plt
@@ -60,13 +62,20 @@ def toRGB(data, channels):
         new_data[:,:,i] = data*channels[i]
     return new_data
 
-def simCCD(ccd_fname_prefix, N_ccd, bins, extents,
-           outfile=None, show=True, brightness=1, imgcmd="eog"):
-    """Simulate a CCD image."""
+def simCCD(ccd_file, N_ccd, bins, extents,
+           outfile=None, show=False, brightness=1, imgcmd="eog"):
+    """Simulate a CCD image. If ccd_file is a string, opens N_ccd
+    files named ccd_file + '_<integer>.dat'. If a length N_ccd list of
+    file objects, it reads those file objects instead."""
     tmpimg = "images/tmp.png"
     ccd = np.zeros((bins,bins,3))
     for i in range(N_ccd):
-        hist = np.fromfile(ccd_fname_prefix + "_" + str(i+1) + ".dat")
+        if isinstance(ccd_file, str):
+            hist = np.fromfile(ccd_file + "_" + str(i+1) + ".dat")
+        elif isinstance(ccd_file, list):
+            hist = np.fromstring(ccd_file[i])
+        else:
+            raise TypeError("ccd_file must be a string or a list of file objects.")
         ranges = hist[:(bins*2+2)]
         data = hist[(bins*2+2):]
         data /= data.max()
@@ -82,8 +91,51 @@ def simCCD(ccd_fname_prefix, N_ccd, bins, extents,
         imgC.show(command=imgcmd)
     return ccd
 
+def simCCDArchive(tar_fname, N_ccd, bins, extents, **kwargs):
+    """
+    Open a tar archive containing a bunch of simulated CCD data
+    files. The data files in the archive must have a name of the
+    form::
+
+      <prefix>_<integer>.dat
+
+    where `<prefix>` is some number or otherwise distinguising bit of
+    the file name in order to differentiate between simulation runs
+    and the integer following the underscore is (inclusively) between
+    1 and N_ccd and indicates the CCD for a particular ion
+    species. For example, an archive might contain the following
+    filenames::
+    
+      ['fraction0.3ccd_1.dat',
+       'fraction0.3ccd_2.dat',
+       'fraction0.3ccd_3.dat',
+       'fraction0.4ccd_1.dat',
+       'fraction0.4ccd_2.dat',
+       'fraction0.4ccd_3.dat]
+
+    In this case, there are two `<prefix>` values: `fraction0.3ccd`
+    and `fraction0.4ccd` which correspond to different simulation
+    runs, and there being three of each indicates there were 3
+    simulated CCDs for 3 different ion species.
+
+    TODO: add kwargs for things to go to simCCDx
+    """
+    tar = tarfile.open(tar_fname, 'r')
+    names, members = tar.getnames(), tar.getmembers()
+    prefixes = sorted(set([x.split('_')[0] for x in names]))
+    for prefix in prefixes:
+        ccd_file = []
+        for i in range(N_ccd):
+            data = tar.extractfile(prefix + "_%i.dat" % (i+1)).read()
+            ccd_file.append(data)
+        simCCD(ccd_file, N_ccd, bins, extents,
+               outfile=(os.path.dirname(tar_fname)+"/"+prefix+'.png'))
+    tar.close()
+
 if __name__ == "__main__":
-    ccd_bins, ccd_extent = 512, 600
-    display()
+    #ccd_bins, ccd_extent = 512, 600
+    #display()
     #simCCD("ccd", 2, ccd_bins, ccd_extent, brightness=1.5,
     #       outfile="images/CCD_latest.png", show=True)
+    bins, extents = 768, 1024
+    simCCDArchive("data/N3/ccd/ccddat.tar.gz", 2, bins, extents)
