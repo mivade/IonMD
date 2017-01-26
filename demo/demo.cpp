1751d513041e64d82dbe781b075079c0cae65e6e
Michael V. DePalatis <mike@depalatis.net>
2017-01-25 19:03:49 -0500

    Rearrange for multiple CMake targets

---
+++ demo/demo.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <ionmd/ion.hpp>
+
+using std::cout;
+using std::endl;
+using namespace ionmd;
+
+typedef std::vector<Ion> ions_t;
+
+
+int main(int argc, char *argv[]) {
+    cout << "IonMD demo\n" << "==========" << endl;
+
+    std::vector<Ion> ions;
+
+    return 0;
+}
