b2fa814928b2c6959480555ca610f20d9a6c36af
Michael V. DePalatis <mike@depalatis.net>
2017-01-25 19:24:13 -0500

    Add simple two ion demo
    Doesn't link yet due to OpenMP misconfiguration in CMake.

--- demo/demo.cpp
+++ demo/demo.cpp
@@ -1,18 +1,45 @@
 #include <iostream>
 #include <vector>
+
+#include <armadillo>
+
+#include <ionmd/ionmd.hpp>
+#include <ionmd/params.hpp>
+#include <ionmd/trap.hpp>
 #include <ionmd/ion.hpp>
 
 using std::cout;
 using std::endl;
+using arma::vec;
+
 using namespace ionmd;
 
 typedef std::vector<Ion> ions_t;
 
 
+ions_t make_ions(params_ptr p, trap_ptr trap) {
+    ions_t ions;
+    ions.push_back(Ion(p, trap, 40., 1., vec{{0., 0., -20e-6}}));
+    ions.push_back(Ion(p, trap, 40., 1., vec{{0., 0., 20e-6}}));
+    return ions;
+}
+
+
 int main(int argc, char *argv[]) {
     cout << "IonMD demo\n" << "==========" << endl;
 
-    std::vector<Ion> ions;
+    cout << "Using default simulation and trap parameters..." << endl;
+    auto p = std::make_shared<SimParams>();
+    auto trap = std::make_shared<Trap>();
+
+    cout << "Generating ions..." << endl;
+    std::vector<Ion> ions = make_ions(p, trap);
+
+    Simulation sim(*p.get(), *trap.get(), ions);
+
+    cout << "Running simulation..." << endl;
+    sim.run();
+    cout << "Simulation complete." << endl;
 
     return 0;
 }
